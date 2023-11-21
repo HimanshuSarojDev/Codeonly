927f683c3c0daca4e258452b54846d9778dca342
himanshusaroj <48007478+HimanshuSarojDev@users.noreply.github.com>
2023-11-21 11:15:05 +0530

    stack with list

---
+++ stack.py
@@ -0,0 +1,41 @@
+class stack:
+    def __init__(self):
+        self.list=[]
+ 
+    def isEmpty(self):
+        if self.list == []:
+            return True
+        else:
+            return False
+        
+    def push(self,item):
+        self.list.append(item)
+        return "values is appended"
+    
+    def pop(self):
+        if self.isEmpty():
+            return "no element in stack"
+        else:
+            self.list.pop()
+            return "values is poped"
+    def peek(self):
+        if self.isEmpty():
+            return "no element in stack"
+        else:
+            return print(self.list[len(self.list)-1])
+    
+    def print_stack(self):
+        print("Stack:", self.list)
+        
+    def delete(self):
+        self.list = None
+
+        
+        
+custom = stack()
+custom.push(90)
+custom.push(30)
+custom.push(50)
+custom.push(10)
+custom.print_stack()
+
